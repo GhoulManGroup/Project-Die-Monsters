f500fca3889e7c42a200dc111d501b800bb62b3a
Jack.Russell-Brindle <Jack.russell-brindle@mxreality.co.uk>
2022-10-18 08:47:21 +0100

    test

--- Project-Die-Monsters/Assets/Scripts/Other/Scriptable Objects/DungeonLord.cs
+++ Project-Die-Monsters/Assets/Scripts/Other/Scriptable Objects/DungeonLord.cs
@@ -5,13 +5,6 @@ using UnityEngine;
 [CreateAssetMenu]
 public class DungeonLord : ScriptableObject
 {
-    //Dungeon Lord Stuff ? 
-    //Damage ? | Health | Ability ? | Card Sprite
 
     public Sprite CardArt;
-
-    int startingHealth;
-    int attackDamage;
-
-    
 }
