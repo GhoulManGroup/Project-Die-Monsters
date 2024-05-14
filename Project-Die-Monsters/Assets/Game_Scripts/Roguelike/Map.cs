4504e5a8103dc1b71087b72d2b29a1b6069388a7
MXREALITY\Jack.Russell-Brindle <Jack.Russell-Brindle@mxreality.co.uk>
2024-05-14 14:04:52 +0100

    14/05/2024 : created new encouter object to be spawned, started thinking about how the map should be created so far thnking randomly instacating nodes based on width / height values .

--- Project-Die-Monsters/Assets/Game_Scripts/Roguelike/Map.cs
+++ Project-Die-Monsters/Assets/Game_Scripts/Roguelike/Map.cs
@@ -11,12 +11,35 @@ public class Map : MonoBehaviour
     [SerializeField] int mapWidthLimit;
 
     [SerializeField] int rowWidth;
-                
+
+    [SerializeField] GameObject spawnNode;
+    public List<Encounter> encounters = new List<Encounter>();
 
     public void SetDetails()
     {
         mapHeightLimit = 4 + this.GetComponent<RunManager>().runProgress;
-        mapWidthLimit = Random.Range(1, this.GetComponent<RunManager>().runProgress + 1);
+        //mapWidthLimit = 1 + this.get
+    }
+
+    public void SpawnMap()
+    {
+        for (int i = 0; i < mapHeightLimit; i++)
+        {
+            if (i == 0)
+            {
+                // spawn in start spot
+            }
+            else if (i == mapHeightLimit)
+            {
+                // spawn 1 in end spot Random.Range(1, this.GetComponent<RunManager>().runProgress + 1);
+            }
+            else
+            {
+                // spawn multiple nodes = m
+            }
+
+
+        }
     }
 }
     
