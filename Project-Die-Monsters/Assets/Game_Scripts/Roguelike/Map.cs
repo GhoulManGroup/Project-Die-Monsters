e8416806c3aff7fb111beb2b39a1eb03b715b206
MXREALITY\Jack.Russell-Brindle <Jack.Russell-Brindle@mxreality.co.uk>
2024-05-20 14:09:46 +0100

    no message

--- Project-Die-Monsters/Assets/Game_Scripts/Roguelike/Map.cs
+++ Project-Die-Monsters/Assets/Game_Scripts/Roguelike/Map.cs
@@ -10,34 +10,46 @@ public class Map : MonoBehaviour
 
     [SerializeField] int mapWidthLimit;
 
-    [SerializeField] int rowWidth;
-
     [SerializeField] GameObject spawnNode;
     public List<Encounter> encounters = new List<Encounter>();
 
+    RunManager runManager;
+
+    private void Start()
+    {
+        runManager = GameObject.FindGameObjectWithTag("RunManager").GetComponent<RunManager>() ;
+        SetDetails();
+        SpawnMap();
+    }
+
     public void SetDetails()
     {
-        mapHeightLimit = 4 + this.GetComponent<RunManager>().runProgress;
-        mapWidthLimit = 1 + this.GetComponent<RunManager>().runProgress;
+        mapHeightLimit = 4 + runManager.runProgress;
+        mapWidthLimit = 1 + runManager.runProgress;
     }
 
     public void SpawnMap()
     {
+        List<int> usedPositons = new List<int>();
+
         for (int i = 0; i < mapHeightLimit; i++)
         {
             if (i == 0)
             {
                 // spawn in start spot
-                Instantiate(spawnNode, new Vector3(i * 0, i, 0), Quaternion.identity);
+                Instantiate(spawnNode, new Vector3(mapWidthLimit / 2, i, 0), Quaternion.identity);
             }
             else if (i == mapHeightLimit)
             {
                 // spawn 1 in end spot Random.Range(1, this.GetComponent<RunManager>().runProgress + 1);
-                Instantiate(spawnNode, new Vector3(i * 0, mapHeightLimit, 0), Quaternion.identity);
+                Instantiate(spawnNode, new Vector3(mapWidthLimit / 2, mapHeightLimit, 0), Quaternion.identity);
             }
             else
             {
-                // spawn multiple nodes = m
+                for (int j = 0; j < mapWidthLimit; j++)
+                {
+                    Instantiate(spawnNode, new Vector3(i, mapHeightLimit, 0), Quaternion.identity);
+                }
             }
 
 
