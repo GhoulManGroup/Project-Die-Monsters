048f5a14b6766c3ace6453b4557da8ee21e8896d
MXREALITY\Jack.Russell-Brindle <Jack.Russell-Brindle@mxreality.co.uk>
2024-06-03 09:28:28 +0100

    Adding Missing Files from source controls filter settings hiding new files.

---
+++ Project-Die-Monsters/Assets/Game_Scripts/Roguelike/MapEncounter.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapEncounter : MonoBehaviour
+{
+    public List<GameObject> myConnections = new List<GameObject>();
+
+    public Encounter myEncounter;
+
+    public List<Sprite> encounterIcon = new List<Sprite>();
+
+    public void Start()
+    {
+        //Set sprites here among other setups
+    }
+}
